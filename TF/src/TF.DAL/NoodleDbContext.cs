f6907c301536d465feb5454bd865d0459d3f4d0d
Sergey Dushkin <leirby@live.ru>
2016-03-20 02:15:02 +0300

    add ef

---
+++ TF/src/TF.DAL/NoodleDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using TF.Data.Business;
+using TF.Data.Business.WMS;
+
+namespace TF.DAL
+{
+    class NoodleDbContext : DbContext
+    {
+        public NoodleDbContext() : base("NoodleDb") { }
+        public NoodleDbContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Price> Prices { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("[BUSINESS.WMS.PRODUCT_N_SERVICE]");
+            modelBuilder.Entity<Product>().HasKey(r => r.Id);
+            modelBuilder.Entity<Product>().Property(r => r.Id)
+                .HasColumnName("GUID_RECORD")
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<Product>().Property(r => r.Name).HasColumnName("NAME").IsRequired().HasMaxLength(20);
+            modelBuilder.Entity<Product>().Property(r => r.Key).HasColumnName("KEY").IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<Product>().Property(r => r.Type).HasColumnName("TYPE").IsRequired().HasMaxLength(200);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(r => r.ChildProducts)
+                .WithRequired(r => r.Parent);
+
+        }
+    }
+}
---
+++ TF/src/TF.DAL/ProdutService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TF.Data.Business.WMS;
+
+namespace TF.DAL
+{
+    public class ProductService : IProductService
+    {
+        public void Delete(Guid id)
+        {
+            Product entity = null;
+            using (var context = new NoodleDbContext())
+            {
+                entity = context.Products.Include("ChildProducts").SingleOrDefault(p => p.Id == id);
+                if (entity != null)
+                {
+                    context.Products.Remove(entity);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            using (var context = new NoodleDbContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public Product GetById(Guid id)
+        {
+            using (var context = new NoodleDbContext())
+            {
+                return context.Products.Include("ChildProducts").SingleOrDefault(p => p.Id == id);
+            }
+        }
+
+        public Product Update(Product product)
+        {
+            using (var context = new NoodleDbContext())
+            {
+                context.Products.Attach(product);
+                context.Entry(product).State = EntityState.Modified;
+                context.SaveChanges();
+
+                return product;
+            }
+        }
+
+        public Product Create(Product product)
+        {
+            using (var context = new NoodleDbContext())
+            {
+                var result = context.Products.Add(product);
+                context.SaveChanges();
+
+                return context.Products.Add(result);
+            }
+        }
+    }
+}

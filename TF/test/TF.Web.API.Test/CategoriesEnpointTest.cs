6b8097d3c18f773175611c9ae4cf3d2279c2b145
Sergey Dushkin <leirby@live.ru>
2016-03-20 16:04:23 +0300

    add CategoryCrudTest

---
+++ TF/test/TF.Web.API.Test/CategoriesEnpointTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TF.Web.API.Test.NoodleService;
+using TF.Data.Business;
+using System.Data.Services.Client;
+using System.Linq;
+
+namespace TF.Web.API.Test
+{
+    [TestClass]
+    public class CategoriesEnpointTest
+    {
+        [TestMethod]
+        public void GetCategoriesTest()
+        {
+            var container = new Container(new Uri("http://localhost:5588/odata/"));
+
+            var data = container.Categories;
+
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        public void CategoryCrudTest()
+        {
+            var container = new Container(new Uri("http://localhost:5588/odata/"));
+
+            container.AddToCategories(new Category
+            {
+                Key = "TestCategory",
+                Name = "TestCategory"
+            });
+
+            var responses = container.SaveChanges();
+
+            foreach (var response in responses)
+            {
+                var changeResponse = (ChangeOperationResponse)response;
+                var entityDescriptor = (EntityDescriptor)changeResponse.Descriptor;
+                var entity = (Category)entityDescriptor.Entity;
+
+                var entity2 = container.Categories
+                    .Single(r => r.Id == entity.Id);
+
+                container.DeleteObject(entity);
+                var deleteResponses = container.SaveChanges();
+
+                Assert.IsNotNull(entity2);
+                Assert.IsNotNull(deleteResponses);
+            }
+
+
+            Assert.IsNotNull(responses);
+        }
+    }
+}

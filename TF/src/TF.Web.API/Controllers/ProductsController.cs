feb24b3ead7d973386fe3c7986192df657a4f128
Sergey Dushkin <leirby@live.ru>
2016-03-20 21:16:34 +0300

    add ProductsController

---
+++ TF/src/TF.Web.API/Controllers/ProductsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Data.OData;
+using NLog;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Query;
+using TF.Data.Business.WMS;
+
+namespace TF.Web.API.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        private static ODataValidationSettings _validationSettings = new ODataValidationSettings();
+
+        private readonly IProductService service;
+        private readonly IProductPriceService productPriceService;
+        private readonly IProductCategoryService productCategoryService;
+        private readonly ILogger logger;
+
+        public ProductsController(
+            IProductService service,
+            IProductPriceService productPriceService,
+            IProductCategoryService productCategoryService,
+            ILogger logger)
+        {
+            this.service = service;
+            this.productPriceService = productPriceService;
+            this.productCategoryService = productCategoryService;
+            this.logger = logger;
+
+            this.logger.Trace("Call ProductsController");
+        }
+
+        [EnableQuery]
+        public async Task<IHttpActionResult> GetProducts(ODataQueryOptions<Product> queryOptions)
+        {
+            logger.Trace("Call ProductsController GetProducts");
+
+            try
+            {
+                queryOptions.Validate(_validationSettings);
+            }
+            catch (ODataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            var data = service.Get();
+
+            var query = (IQueryable<Product>)queryOptions
+                .ApplyTo(data.AsQueryable());
+
+            return Ok(query);
+        }
+
+        public async Task<IHttpActionResult> GetProduct([FromODataUri] System.Guid key)
+        {
+            logger.Trace("Call ProductsController GetProduct");
+
+            var query = service.GetById(key);
+
+            return Ok(query);
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] Product entity)
+        {
+            logger.Trace("Call ProductsController Post");
+
+            var record = service.Create(entity);
+            return Created<Product>(record);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> Put([FromBody] Product entity)
+        {
+            logger.Trace("Call ProductsController Put");
+
+            var record = service.Update(entity);
+            return Updated<Product>(record);
+        }
+
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete([FromODataUri] System.Guid key)
+        {
+            logger.Trace("Call ProductsController Delete");
+
+            service.Delete(key);
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            logger.Trace("End ProductsController");
+
+            base.Dispose(disposing);
+        }
+    }
+}

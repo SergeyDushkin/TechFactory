dbf23ded14e842d1bff24dcf7ab1a438a0800fd0
Sergey Dushkin <leirby@live.ru>
2016-03-22 21:21:01 +0300

    add missed files

---
+++ TF/src/TF.DAL/ProductCategoryService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using TF.Data.Business.WMS;
+using TF.DAL.Query;
+using TF.Data.Business;
+
+namespace TF.DAL
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        private readonly NoodleDbContext context;
+
+        public ProductCategoryService(NoodleDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(Guid id)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                connection.Execute(ProductCategoryQuery.Delete(id));
+            }
+        }
+
+        public IEnumerable<ProductCategory> GetByCategoryId(Guid id)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                return connection.Query<ProductCategory>(ProductCategoryQuery.ByCategoryId(id));
+            }
+        }
+
+        public IEnumerable<ProductCategory> GetByProductId(Guid id)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                return connection.Query<ProductCategory>(ProductCategoryQuery.ByProductId(id));
+            }
+        }
+
+        public IEnumerable<Product> GetProductsByCategoryId(Guid id)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                return connection.Query<Product>(ProductCategoryQuery.ProductsByCategoryId(id));
+            }
+        }
+
+        public IEnumerable<Category> GetCategoriesByProductId(Guid id)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                return connection.Query<Category>(ProductCategoryQuery.CategoriesByProductId(id));
+            }
+        }
+
+        public ProductCategory GetById(Guid id)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                return connection.Query<ProductCategory>(ProductCategoryQuery.ById(id)).SingleOrDefault();
+            }
+        }
+
+        public ProductCategory Update(ProductCategory productCategory)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                connection.Execute(ProductCategoryQuery.Update(productCategory));
+                return productCategory;
+            }
+        }
+
+        public ProductCategory Create(ProductCategory productCategory)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                connection.Execute(ProductCategoryQuery.Insert(productCategory));
+                return productCategory;
+            }
+        }
+    }
+}

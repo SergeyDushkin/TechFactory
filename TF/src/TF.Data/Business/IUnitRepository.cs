a64077e82ffddaaa72831f3ebed7421cbbfb08b2
Sergey Dushkin <leirby@live.ru>
2016-03-31 00:18:19 +0300

    Add Unit class

---
+++ TF/src/TF.Data/Business/IUnitRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TF.Data.Business
+{
+    public interface IUnitRepository
+    {
+        Unit Create(Unit unit);
+        Task<Unit> CreateAsync(Unit unit);
+
+        Unit Update(Unit unit);
+        Task<Unit> UpdateAsync(Unit unit);
+
+        void Delete(Guid id);
+        Task DeleteAsync(Guid id);
+
+        IEnumerable<Unit> GetAll();
+        IEnumerable<Unit> GetByParentId(Guid id);
+        Unit GetById(Guid id);
+
+        Task<IEnumerable<Unit>> GetAllAsync();
+        Task<IEnumerable<Unit>> GetByParentIdAsync(Guid id);
+        Task<Unit> GetByIdAsync(Guid id);
+    }
+}
---
+++ TF/src/TF.Data/Business/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TF.Data.Business
+{
+    public class Unit
+    {
+        public Guid Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+
+        public Guid? ParentId { get; set; }
+    }
+}

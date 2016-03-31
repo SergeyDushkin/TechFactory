797bfb944093e4b7bcc03fd9f1a0da31fa476f58
Sergey Dushkin <leirby@live.ru>
2016-04-01 00:18:36 +0300

    Add location class

---
+++ TF/src/TF.Data/Business/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TF.Data.Business
+{
+    public class Location
+    {
+        public Guid Id { get; set; }
+        public Guid UnitId { get; set; }
+
+        /// <summary>
+        /// fixed values: STORE, WAREHOUSE, OFFICE, HOME, WORK, MAIN
+        /// </summary>
+        public string Type { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+
+        public virtual Unit Unit { get; set; }
+    }
+}

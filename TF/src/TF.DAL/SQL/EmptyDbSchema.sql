471715ac63b02e77f58a5a76bbb8dba715cb17bd
Sergey Dushkin <leirby@live.ru>
2016-04-03 00:12:53 +0300

    Add location

--- TF/src/TF.DAL/SQL/EmptyDbSchema.sql
+++ TF/src/TF.DAL/SQL/EmptyDbSchema.sql
@@ -64,6 +64,18 @@ CREATE TABLE [dbo].[BUSINESS.UOM](
 	[DELETED] [bit] NOT NULL
 ) ON [PRIMARY]
 
+/*  table to store physical presense, such as STORE, WAREHOUSE, etc */
+CREATE TABLE [BUSINESS.LOCATION](
+	[GUID_RECORD] [uniqueidentifier] NOT NULL,
+	[BUSINESSUNIT_GUID] [uniqueidentifier] NOT NULL,
+	[TYPE] [nvarchar](20) NOT NULL,							-- fixed values: STORE, WAREHOUSE, OFFICE, HOME, WORK, MAIN
+	[KEY] [nvarchar](50) NOT NULL,
+	[NAME] [nvarchar](200),
+	[BATCH_GUID] [uniqueidentifier],
+	[HIDDEN] [bit] NOT NULL,
+	[DELETED] [bit] NOT NULL
+) ON [PRIMARY]
+
 /* table to store trees data (parent-child) */
 CREATE TABLE [dbo].[BUSINESS.CATEGORY_TREE](
 	[GUID_RECORD] [uniqueidentifier] NOT NULL,
---
+++ TF/test/TF.DAL.Test/LocationRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TF.Data.Business;
+
+namespace TF.DAL.Test
+{
+    [TestClass]
+    public class LocationRepositoryTest
+    {
+        [TestMethod]
+        public void LocationRepositoryCRUDTest()
+        {
+            var context = new NoodleDbContext("NoodleDb");
+            context.Init();
+
+            ILocationRepository repository = new LocationRepository(context);
+
+            var id = Guid.NewGuid();
+
+            var record = new Location
+            {
+                Id = id,
+                UnitId = Guid.Empty,
+                Type = "STORE",
+                Key = "Key_" + id.ToString(),
+                Name = "Name_" + id.ToString()
+            };
+
+            repository.Create(record);
+
+            record.Key = "U_" + record.Key;
+            record.Name = "U_" + record.Name;
+            record.Type = "HOME";
+
+            repository.Update(record);
+
+            var record2 = repository.GetById(id);
+
+            Assert.AreEqual(record.Id, record2.Id);
+            Assert.AreEqual(record.Key, record2.Key);
+            Assert.AreEqual(record.Name, record2.Name);
+            Assert.AreEqual(record.Type, record2.Type);
+
+            repository.Delete(record.Id);
+
+            var record3 = repository.GetById(id);
+
+            Assert.IsNull(record3);
+        }
+    }
+}

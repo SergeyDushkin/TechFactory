5db5cd6dd55acc7747d09cbc3452c9e2270ee5ec
SergeyDushkin <leirbythe@gmail.com>
2016-04-04 14:40:48 +0300

    Add Employee class

---
+++ TF/src/TF.Data/Business/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TF.Data.Business
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public Guid UnitId { get; set; }
+        public Guid PersonId { get; set; }
+
+        public virtual Unit Unit { get; set; }
+        public virtual Person Person { get; set; }
+    }
+}
---
+++ TF/src/TF.Data/Business/IEmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TF.Data.Business
+{
+    public interface IEmployeeRepository
+    {
+        Employee Create(Employee employee);
+        Task<Employee> CreateAsync(Employee employee);
+
+        Employee Update(Employee employee);
+        Task<Employee> UpdateAsync(Employee employee);
+
+        void Delete(Guid id);
+        Task DeleteAsync(Guid id);
+
+        Employee GetById(Guid id);
+        Task<Employee> GetByIdAsync(Guid id);
+
+        IEnumerable<Employee> GetByUnitId(Guid id);
+        Task<IEnumerable<Employee>> GetByUnitIdAsync(Guid id);
+
+        IEnumerable<Employee> GetByPersonId(Guid id);
+        Task<IEnumerable<Employee>> GetByPersonIdAsync(Guid id);
+
+        IEnumerable<Employee> GetAll();
+        Task<IEnumerable<Employee>> GetAllAsync();
+    }
+}

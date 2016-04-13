bef21f123b2a7acdc0b34b6a431ace9e248f3423
Alex My <alexmy@outlook.com>
2016-04-13 23:52:18 +0300

    Some client sample functionality

---
+++ TF/TF.Client.Cordova/www/scripts/locations.directive.js
@@ -0,0 +1,34 @@
+﻿angular.module('locationsApp').directive('locationsList', function (LocationsService) {
+    function controller($scope, $rootScope) {
+
+        getLocations();
+
+        $scope.$on("locationsUpdated", getLocations);
+
+        function getLocations() {
+            LocationsService.query()
+				.then(function (answer) {
+				    $scope.locations = answer.data.value;
+				}, function (answer) {
+				    console.log("Error. Status: " + answer.status + "; StatusText: " + answer.statusText);
+				    return [];
+				});
+        };
+
+        function updateLocations(doc) {
+            LocationsService.save(doc)
+				.then(function (answer) {
+				    $rootScope.$broadcast("locationsUpdated");
+				}, function (answer) {
+				    console.log("Error. Status: " + answer.status + "; StatusText: " + answer.statusText);
+				});
+        };
+
+    }
+
+    return {
+        restrict: "E",
+        templateUrl: "locations.list-template.html",
+        controller: controller
+    };
+});

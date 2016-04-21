7b765f9af227dadc5d61fc098ec1fd973f1ce101
SergeyDushkin <leirbythe@gmail.com>
2016-04-21 17:40:31 +0300

    Add Workflow classes

---
+++ TF/src/TF.Data/Business/Workflow/WorkflowMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TF.Data.Business.Workflow
+{
+    /// <summary>
+    /// table which remains ability of changing statuses in workflow, ie NEW > (OPEN || CANCELED) > CLOSED
+    /// ROOT NODE doesn't have record in this table
+    /// </summary>
+    public class WorkflowMap
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// reference to the workflow status
+        /// </summary>
+        public Guid StatusId { get; set; }
+
+        /// <summary>
+        /// reference to the parent status
+        /// </summary>
+        public Guid ParentStatusId { get; set; }
+    }
+}
--- TF/src/TF.Web.API/Startup.cs
+++ TF/src/TF.Web.API/Startup.cs
@@ -3,10 +3,13 @@ using Microsoft.Practices.Unity;
 using NLog;
 using Owin;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.OData.Builder;
 using System.Web.Http.OData.Extensions;
+using System.Web.Http.Tracing;
 using TF.DAL;
 using TF.Data.Business;
 using TF.Data.Business.WMS;
@@ -30,6 +33,9 @@ namespace TF.Web.API
             Startup.RegisterOdataRoutes(config);
 
             config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            /// Trace
+            /// config.Services.Replace(typeof(ITraceWriter), new CustomTracer());
 
             ///// Подключаем модуль web api
             app.UseWebApi(config);
@@ -100,5 +106,60 @@ namespace TF.Web.API
             application.Dispose();
         }
     }
-    
+
+    //public class CustomTracer : ITraceWriter
+    //{
+    //    private readonly Logger logger;
+
+    //    private static readonly Lazy<Dictionary<TraceLevel, Action<string>>> loggingMap =
+    //        new Lazy<Dictionary<TraceLevel, Action<string>>>(() => new Dictionary<TraceLevel, Action<string>>
+    //            {
+    //                {TraceLevel.Info, LogManager.GetCurrentClassLogger().Info },
+    //                {TraceLevel.Debug, LogManager.GetCurrentClassLogger().Debug},
+    //                {TraceLevel.Error, LogManager.GetCurrentClassLogger().Error},
+    //                {TraceLevel.Fatal, LogManager.GetCurrentClassLogger().Fatal},
+    //                {TraceLevel.Warn, LogManager.GetCurrentClassLogger().Warn}
+    //            });
+
+
+    //    public CustomTracer()
+    //    {
+    //        logger = LogManager.GetCurrentClassLogger();
+    //    }
+
+    //    public void Trace(HttpRequestMessage request, string category, TraceLevel level,
+    //        Action<TraceRecord> traceAction)
+    //    {
+    //        TraceRecord rec = new TraceRecord(request, category, level);
+
+    //        traceAction(rec);
+    //        WriteTrace(rec);
+    //    }
+
+    //    protected void WriteTrace(TraceRecord rec)
+    //    {
+    //        var message = string.Format("{0};{1};{2}",
+    //            rec.Operator, rec.Operation, rec.Message);
+    //        System.Diagnostics.Trace.WriteLine(message, rec.Category);
+
+    //        var log = new LogEventInfo(LogLevel.Info, "", message);
+
+    //        log.Properties["CorrelationId"] = rec.Request.GetCorrelationId();
+
+    //        if (rec.Level == TraceLevel.Info)
+    //            logger.Info(message);
+    //    }
+    //}
+
+    //public interface IEventWriter
+    //{
+    //    string CorrelationId { get; set; }
+
+    //    void Log();
+    //    void Trace();
+    //    void Event();
+    //    void Exception();
+    //    void Profile();
+    //}
+
 }
